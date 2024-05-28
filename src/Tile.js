e2d55738152a4abffaef19a6815548dd34da34ad
Benjamin Bates <bbates@gopds.net>
2024-05-28 15:39:35 -0400

    Fixed error in Tile.js

--- src/Tile.js
+++ src/Tile.js
@@ -38,7 +38,7 @@ export default class Tile {
 		this.#tileElement.remove()
 	}
 	
-	waitForTransition() {
+	waitForTransition(animation = false) {
 		return new Promise(resolve => {
 			this.#tileElement.addEventListener(animation ? "animationend" : "transitionend", resolve, {
 				once: true,
