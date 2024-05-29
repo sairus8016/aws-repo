c2cecf36668852705dca5a3114a77ac9272eef1f
Benjamin Bates <bbates@gopds.net>
2024-05-29 14:25:26 -0400

    Add touch event listeners

--- src/App.js
+++ src/App.js
@@ -62,6 +62,8 @@ function App({ signOut }) {
   
   useEffect(() => {
 	const gameBoard = document.getElementById("game-board");
+	var xDown = null;
+	var yDown = null;
 	
 	const grid = new Grid(gameBoard);
 	grid.randomEmptyCell().tile = new Tile(gameBoard);
@@ -70,7 +72,65 @@ function App({ signOut }) {
 	
 	function setupInput() {
 		window.addEventListener("keydown", handleInput, { once: true });
+		window.addEventListener('touchstart', handleTouchStart, { once: true });
+		window.addEventListener('touchmove', handleTouchMove, { once: true });
 	}
+	
+	function getTouches(evt) {
+		return evt.touches ||             // browser API
+			evt.originalEvent.touches; // jQuery
+	}                                                     
+															   
+	function handleTouchStart(evt) {
+		const firstTouch = getTouches(evt)[0];                                      
+		xDown = firstTouch.clientX;                                      
+		yDown = firstTouch.clientY;                                      
+	};                                                
+															   
+	async function handleTouchMove(evt) {
+		if ( ! xDown || ! yDown ) {
+			return;
+		}
+
+		var xUp = evt.touches[0].clientX;                                    
+		var yUp = evt.touches[0].clientY;
+
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+															   
+		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+			if ( xDiff > 0 ) {
+				if (!canMoveRight()) {
+					setupInput();
+					return;
+				}
+				await moveRight();
+			} else {
+				if (!canMoveLeft()) {
+					setupInput();
+					return;
+				}
+				await moveLeft();
+			}                       
+		} else {
+			if ( yDiff > 0 ) {
+				if (!canMoveDown()) {
+					setupInput();
+					return;
+				}
+				await moveDown();
+			} else { 
+				if (!canMoveUp()) {
+					setupInput();
+					return;
+				}
+				await moveUp();
+			}                                                                 
+		}
+		/* reset values */
+		xDown = null;
+		yDown = null;                                             
+	};
 	  
 	async function handleInput(e) {
 		switch (e.key) {
